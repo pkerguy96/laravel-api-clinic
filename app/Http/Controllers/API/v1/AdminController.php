345f1abf86b4a011d6cffe64780708bba1167a3a
pkerguy96 <aymenelkor@hotmail.com>
2023-08-08 01:14:21 +0100

    bug fixes

--- app/Http/Controllers/API/v1/AdminController.php
+++ app/Http/Controllers/API/v1/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\v1;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -13,13 +14,12 @@ class AdminController extends Controller
     public function storeprofile(Request $request)
     {
         $request->validate([
-            'picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:5000',
         ]);
-
         $user = auth()->user();
         if ($user->profile_picture) {
             // Delete the existing profile picture
-            Storage::disk('public')->delete($user->profile_picture);
+            Storage::disk('public')->delete('profile_pictures/' . $user->profile_picture);
         }
         if ($request->hasFile('picture') && $request->file('picture')->isValid()) {
             $extension = $request->file('picture')->getClientOriginalExtension();
@@ -34,6 +34,39 @@ class AdminController extends Controller
         }
 
         return response()->json(['message' => 'Failed to upload profile picture'], 400);
+    }
+
+    public function ModifyProfile(Request $request)
+    {
+
+        $userid = Auth::id();
+        $user = user::findorfail($userid);
+        $oldProfilePicture = $user->profile_picture;
+        if ($request->hasFile('picture') && $request->file('picture')->isValid()) {
+            $request->validate([
+                'picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:5000',
+            ]);
+            if ($oldProfilePicture) {
+                // Delete the existing profile picture
+                Storage::disk('public')->delete('profile_pictures/' . $user->profile_picture);
+            }
+            $extension = $request->file('picture')->getClientOriginalExtension();
+            $fileName = 'user_' . $user->id . '_' . Str::random(10) . '.' . $extension;
+            $request->file('picture')->storeAs('profile_pictures', $fileName, 'public');
+            $user->profile_picture = $fileName;
+        }
+        $url = asset("storage/profile_pictures/"  . $user->profile_picture);
+
+        // Update user attributes
+        $user->name = $request->name;
+        $user->email = $request->email;
+        //Save the user model to update the database
+        $user->save();
+        return response()->json([
+            'message' => 'Profile  Updated successfully',
+            'data' => $user,
+            'profile' => $url
+        ]);
     }
     public function getpicture()
     {
